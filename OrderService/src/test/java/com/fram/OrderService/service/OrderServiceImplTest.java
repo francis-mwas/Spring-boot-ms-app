d737ff3ec5548a9f031d709a13e960744d9ebddd
francis-mwas <mwangif184@gmail.com>
2023-06-20 23:13:17 +0300

    adding unit tests

---
+++ OrderService/src/test/java/com/fram/OrderService/service/OrderServiceImplTest.java
@@ -0,0 +1,7 @@
+package com.fram.OrderService.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+}
--- serviceRegistry/src/main/java/com/fram/serviceRegistry/ServiceRegistryApplication.java
+++ serviceRegistry/src/main/java/com/fram/serviceRegistry/ServiceRegistryApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer //to make it work as a registry server
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
